a01ca745a3a0207962519c92c7b70b3d44b878f5
Sarthak Srivastava <54740977+Sarthak-Srivastava03@users.noreply.github.com>
2025-02-18 19:06:37 +0530

    Update main.tf
    Removed backend configuration to keep the first script simple\

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  backend "s3" {
-    encrypt = true
-    bucket  = "307946651452-terraform-states"
-    key     = "development/service-name.tfstate"
-    region  = "us-west-2"
-    dynamodb_table = "terraform-lock"
- }
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
